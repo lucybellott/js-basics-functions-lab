bbb06737bbd1d088b231437cbcf3e4bce67f198c
Lucy <lucybellott@gmail.com>
2021-05-19 11:44:08 -0400

    commit tree 2ee131ddb3b6ddf9ab4cb8405a3bac0d03529af9

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(num) {
+    if (num > 42) {
+    return num - 42
+    }
+    else {
+        return 42-num
+    }
+}
+distanceFromHqInBlocks(43);
+distanceFromHqInBlocks(50);
+distanceFromHqInBlocks(34);
+
+function distanceFromHqInFeet(num) {
+    return distanceFromHqInBlocks(num) * 264
+}
+distanceFromHqInFeet(43);
+
+function distanceTravelledInFeet(numStart,numFinish) {
+    if (numStart > numFinish) {
+        return (numStart - numFinish) * 264
+    }
+    else{
+        return (numFinish - numStart) * 264
+    }
+    }
+    distanceTravelledInFeet(43, 48);
+
+function calculatesFarePrice(num) {
+    if (num < 400) {
+        return 0
+    }
+    else if (num < 2000){
+        return (num - 400) * 0.02
+    }
+    else if(num <=2500) {
+        return 25
+    }
+    else {
+        return "cannot travel that far"
+    }
+    
+}
+
+calculatesFarePrice(distanceFromHqInFeet(43,44));
+calculatesFarePrice(distanceFromHqInFeet(34,32));
